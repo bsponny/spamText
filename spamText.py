30b7d8016ef4b7a69980646c94961b0f375cc510
Brock Sponenburgh <bsponenburgh9815@gmail.com>
2020-05-05 00:46:48 -0600

    added docs folder and makes it read txt files

--- spamText.py
+++ spamText.py
@@ -1,19 +1,17 @@
-## Dependancies: pyautogui, Python3
-## Pyautogui: https://pyautogui.readthedocs.io/en/latest/install.html
-## Python3: https://www.python.org/downloads/
+# Pyautogui: https://pyautogui.readthedocs.io/en/latest/install.html
+
 import pyautogui
 import time
 
-## Replace this with what you would like to spam one word at a time.
-words = "This is the test string"
-
-## this is used to give you time to open up the messages app and click into the text box.
+# this is used to give you time to open up the messages app and click into the text box.
 time.sleep(3)
 
-## Split the "Lyrics" string into individual words
-word = words.split()
-x = range(len(word))
-for i in word:
-    pyautogui.write(i)
-    pyautogui.press('enter')
-    time.sleep(0.001)
+# Specify what text file to use.
+file = open("docs/test.txt")
+
+for words in file:
+    word = words.split()
+    for i in word:
+        pyautogui.write(i)
+        pyautogui.press('enter')
+        time.sleep(0.001)
